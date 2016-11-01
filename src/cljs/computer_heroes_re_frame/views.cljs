fa9ea5e5a436a45151490f800d0ea358580417db
Joshua Glover <joshua.glover@klarna.com>
2016-11-01 19:25:37 +0100

    Add a sprite

--- src/cljs/computer_heroes_re_frame/views.cljs
+++ src/cljs/computer_heroes_re_frame/views.cljs
@@ -4,4 +4,11 @@
 (defn main-panel []
   (let [name (re-frame/subscribe [:name])]
     (fn []
-      [:div @name " pressed"])))
+      [:div {:style {:position "relative"
+                     :width "1024px"
+                     :height "400px"}}
+       [:div @name " pressed"]
+       [:div [:img {:src "img/2tone.jpg"
+                    :width "100px"
+                    :style {:position "absolute"
+                            :bottom "0px"}}]]])))
